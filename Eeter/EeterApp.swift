4dd7526de0af99f5945bc576c60d2a964a4fa0c9
GevorkChalikyan <72428068+GevorkChalikyan@users.noreply.github.com>
2023-11-17 20:18:50 -0800

    Initial Commit

---
+++ Eeter/EeterApp.swift
@@ -0,0 +1,32 @@
+//
+//  EeterApp.swift
+//  Eeter
+//
+//  Created by Gev Chalikyan on 11/17/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EeterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

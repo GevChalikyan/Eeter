9a468b09c9543721eaa0883f711b92d837c009c4
GevChalikyan <72428068+GevorkChalikyan@users.noreply.github.com>
2023-11-29 18:58:35 -0800

    Added a struct object to create a sine wave
     - Source: https://github.com/RedDragonJ/Swift-Learning/tree/main/Animations/Animations/Animations
     - Produces a sine wave with a filled lower half
     - Accepts two member variables:
        * var offset: Angle — Sets the starting position of the sine wave
        * var percent: Double — Sets the vertical height of the sine wave relative to it's frame

---
+++ Eeter/WaveAnimation.swift
@@ -0,0 +1,74 @@
+//
+//  WaveAnimation.swift
+//  Animations
+//
+//  Created by James Layton on 8/7/23.
+//
+
+import SwiftUI
+
+//
+//	Wave animation demo playground
+//
+struct WaveAnimation: View {
+    
+    @State private var percent = 50.0
+    @State private var waveOffset = Angle(degrees: 0)
+    
+    var body: some View {
+        ZStack {
+            Wave(offSet: Angle(degrees: waveOffset.degrees), percent: percent)
+                .fill(Color.blue)
+                .ignoresSafeArea(.all)
+        }
+        .onAppear {
+			withAnimation(.linear(duration: 1.7).repeatForever(autoreverses: false)) {
+                self.waveOffset = Angle(degrees: 360)
+            }
+        }
+    }
+}
+
+struct Wave: Shape {
+    
+    var offSet: Angle
+    var percent: Double
+    
+    var animatableData: Double {
+        get { offSet.degrees }
+        set { offSet = Angle(degrees: newValue) }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var p = Path()
+        
+        let lowestWave = 0.02
+        let highestWave = 1.00
+        
+        let newPercent = lowestWave + (highestWave - lowestWave) * (percent / 100)
+        let waveHeight = 0.015 * rect.height
+        let yOffSet = CGFloat(1 - newPercent) * (rect.height - 4 * waveHeight) + 2 * waveHeight
+        let startAngle = offSet
+        let endAngle = offSet + Angle(degrees: 360 + 10)
+        
+        p.move(to: CGPoint(x: 0, y: yOffSet + waveHeight * CGFloat(sin(offSet.radians))))
+        
+        for angle in stride(from: startAngle.degrees, through: endAngle.degrees, by: 5) {
+            let x = CGFloat((angle - startAngle.degrees) / 360) * rect.width
+            p.addLine(to: CGPoint(x: x, y: yOffSet + waveHeight * CGFloat(sin(Angle(degrees: angle).radians))))
+        }
+        
+        p.addLine(to: CGPoint(x: rect.width, y: rect.height))
+        p.addLine(to: CGPoint(x: 0, y: rect.height))
+        p.closeSubpath()
+        
+        return p
+    }
+}
+
+
+struct WaveAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        WaveAnimation()
+    }
+}
